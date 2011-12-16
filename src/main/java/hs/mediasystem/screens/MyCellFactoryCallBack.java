3c959a48fc037b1564358472cf024b01e8aac81a
John Hendrikx <hjohn@xs4all.nl>
2011-12-16 14:46:52 +0100

    Test for dialog screen

---
+++ src/main/java/hs/mediasystem/screens/MyCellFactoryCallBack.java
@@ -0,0 +1,22 @@
+package hs.mediasystem.screens;
+
+import hs.mediasystem.screens.DialogScreen.Option;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.util.Callback;
+
+public class MyCellFactoryCallBack implements Callback<ListView<Option>, ListCell<Option>> {
+
+  @Override
+  public ListCell<Option> call(ListView<Option> listView) {
+    return new ListCell<Option>() {
+      @Override
+      protected void updateItem(Option item, boolean empty) {
+        if(item != null) {
+          setGraphic(item.getControl());
+        }
+      }
+    };
+  }
+
+}
