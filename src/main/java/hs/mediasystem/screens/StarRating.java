b86416620acae3faa87e945d76240e64d73f9136
John Hendrikx <hjohn@xs4all.nl>
2012-01-31 08:47:32 +0100

    Ongoing StarRating problems; small CSS cleanup

--- src/main/java/hs/mediasystem/screens/StarRating.java
+++ src/main/java/hs/mediasystem/screens/StarRating.java
@@ -1,56 +1,52 @@
 package hs.mediasystem.screens;
 
+import javafx.beans.binding.Bindings;
 import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.SimpleDoubleProperty;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.scene.Group;
 import javafx.scene.layout.HBox;
 import javafx.scene.shape.Polygon;
 import javafx.scene.shape.Rectangle;
 
-public class StarRating extends HBox {
+public class StarRating extends Group {
   private final DoubleProperty rating = new SimpleDoubleProperty(0.0);
   public DoubleProperty ratingProperty() { return rating; }
   public double getRating() { return rating.get(); }
   public void setRating(double rating) { this.rating.set(rating); }
 
-  private final Rectangle rectangle = new Rectangle(0, 0, 0, 0); // TODO scale seems to work, but too much work to get it to work for now (with width at 100)
+  private final Rectangle rectangle = new Rectangle(0, 0, 0, 0);
+  private final HBox stars;
+  private final HBox disabledStars;
 
   public StarRating(final double radius, final double innerRadius, final int numberOfPoints) {
     getStyleClass().add("star-rating");
 
     double[] starData = createStarData(radius, innerRadius, numberOfPoints);
 
-    final HBox stars = createStars(starData);
-    final HBox disabledStars = createStars(starData);
+    stars = createStars(starData);
+    disabledStars = createStars(starData);
 
     stars.setClip(rectangle);
     disabledStars.setDisable(true);
 
-    getChildren().add(new Group() {{
-      getChildren().addAll(disabledStars, stars);
-    }});
+    getChildren().addAll(disabledStars, stars);
 
-//    System.out.println(">>> " + rating.multiply(disabledStars.widthProperty()).getDependencies());
+    rectangle.widthProperty().bind(rating.multiply(disabledStars.widthProperty()));
+    rectangle.heightProperty().bind(disabledStars.heightProperty());
+    disabledStars.opacityProperty().bind(Bindings.when(rating.isEqualTo(0.0, 0.0)).then(0.0).otherwise(1.0));
+
+//    ChangeListener<Number> changeListener = new ChangeListener<Number>() {
+//      @Override
+//      public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number value) {
+//        if(disabledStars.getWidth() > 0 && disabledStars.getHeight() > 0) {
+//          stars.setClip(new Rectangle(0, 0, disabledStars.getWidth() * rating.get(), disabledStars.getHeight()));
+//        }
+//      }
+//    };
 //
-//    rectangle.widthProperty().bind(rating.multiply(disabledStars.widthProperty()));
-//    rectangle.heightProperty().bind(disabledStars.heightProperty());
-
-    ChangeListener<Number> changeListener = new ChangeListener<Number>() {
-      @Override
-      public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number value) {
-        // TODO for some reason, a direct bind on rating is simply not working for changing the width property of the clip.
-        // rectangle.scaleXProperty().set(100.0 / disabledStars.getWidth() * rating.get());
-        if(disabledStars.getWidth() > 0 && disabledStars.getHeight() > 0) {
-          stars.setClip(new Rectangle(0, 0, disabledStars.getWidth() * rating.get(), disabledStars.getHeight()));
-        }
-      }
-    };
-
-    rating.addListener(changeListener);
-    disabledStars.widthProperty().addListener(changeListener);
-    disabledStars.heightProperty().addListener(changeListener);
+//    rating.addListener(changeListener);
+//    disabledStars.widthProperty().addListener(changeListener);
+//    disabledStars.heightProperty().addListener(changeListener);
   }
 
   private static HBox createStars(final double[] points) {
