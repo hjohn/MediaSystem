b1f016a6ecf946f947556deda63b383e58e618ff
John Hendrikx <hjohn@xs4all.nl>
2012-02-11 08:32:55 +0100

    Sonar fix

--- src/main/java/hs/mediasystem/db/Downloader.java
+++ src/main/java/hs/mediasystem/db/Downloader.java
@@ -3,23 +3,22 @@ package hs.mediasystem.db;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.MalformedURLException;
 import java.net.URL;
 
 public class Downloader {
-  
+
   public static final byte[] tryReadURL(String urlString) {
     try {
       return readURL(urlString);
     }
-    catch(Exception e) {
+    catch(IOException e) {
       return null;
     }
   }
-  
-  public static final byte[] readURL(String urlString) throws IOException, MalformedURLException {
+
+  public static final byte[] readURL(String urlString) throws IOException {
     URL url = new URL(urlString);
-    
+
     try(InputStream is = url.openStream();
         ByteArrayOutputStream bais = new ByteArrayOutputStream()) {
       byte[] byteChunk = new byte[4096];
