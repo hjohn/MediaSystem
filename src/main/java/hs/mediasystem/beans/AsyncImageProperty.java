769fda5b32e20381e7da714610a9d3b422651bce
John Hendrikx <hjohn@xs4all.nl>
2012-02-19 13:40:20 +0100

    Initial commit of AsyncImageProperty which can load images in the background and then trigger an update to its value

---
+++ src/main/java/hs/mediasystem/beans/AsyncImageProperty.java
@@ -0,0 +1,67 @@
+package hs.mediasystem.beans;
+
+import hs.mediasystem.util.ImageCache;
+import hs.mediasystem.util.ImageHandle;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.concurrent.Service;
+import javafx.concurrent.Task;
+import javafx.concurrent.Worker.State;
+import javafx.scene.image.Image;
+
+public class AsyncImageProperty extends SimpleObjectProperty<Image> {
+  private final ImageLoadService imageLoadService = new ImageLoadService();
+
+  public AsyncImageProperty(final ObservableValue<ImageHandle> imageHandle) {
+    imageLoadService.stateProperty().addListener(new ChangeListener<State>() {
+      @Override
+      public void changed(ObservableValue<? extends State> observable, State oldValue, State value) {
+        if(value == State.SUCCEEDED) {
+          set(imageLoadService.getValue());
+        }
+        if(value == State.FAILED) {
+          set(null);
+        }
+      }
+    });
+
+    imageHandle.addListener(new ChangeListener<ImageHandle>() {
+      @Override
+      public void changed(ObservableValue<? extends ImageHandle> observable, ImageHandle oldValue, ImageHandle value) {
+        synchronized(imageLoadService) {
+          loadImageInBackground(imageHandle.getValue());
+        }
+      }
+    });
+
+    loadImageInBackground(imageHandle.getValue());
+  }
+
+  private void loadImageInBackground(ImageHandle imageHandle) {
+    if(imageHandle != null) {
+      imageLoadService.setImageHandle(imageHandle);
+      imageLoadService.restart();
+    }
+  }
+
+  private static class ImageLoadService extends Service<Image> {
+    private ImageHandle imageHandle;
+
+    public void setImageHandle(ImageHandle imageHandle) {
+      this.imageHandle = imageHandle;
+    }
+
+    @Override
+    protected Task<Image> createTask() {
+      final ImageHandle imageHandle = this.imageHandle;
+
+      return new Task<Image>() {
+        @Override
+        protected Image call() throws Exception {
+          return ImageCache.loadImage(imageHandle);
+        }
+      };
+    }
+  }
+}
