d144ba2e61adf5e8b48c6b5ebe983ee34ee830cf
John Hendrikx <hjohn@xs4all.nl>
2012-02-21 01:00:33 +0100

    Added GroupSet

---
+++ src/main/java/hs/mediasystem/screens/GroupSet.java
@@ -0,0 +1,13 @@
+package hs.mediasystem.screens;
+
+public class GroupSet {
+  private final String title;
+
+  public GroupSet(String title) {
+    this.title = title;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+}
