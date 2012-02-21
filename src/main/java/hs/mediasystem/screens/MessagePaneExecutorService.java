e95bac1e86bfba560b3f5bc35473c8549f5f0a00
John Hendrikx <hjohn@xs4all.nl>
2012-02-21 12:55:31 +0100

    Rewrite to solve a bug where no tasks would be submitted to the executor when the service exited when its queue was not empty.  Tasks are now always submitted to the executor and a background thread will monitor it to determine if the service should be restarted.

--- src/main/java/hs/mediasystem/screens/MessagePaneExecutorService.java
+++ src/main/java/hs/mediasystem/screens/MessagePaneExecutorService.java
@@ -4,8 +4,7 @@ import hs.mediasystem.util.LifoBlockingDeque;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Queue;
-import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
@@ -14,14 +13,23 @@ import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.concurrent.Service;
 import javafx.concurrent.Task;
-import javafx.concurrent.Worker.State;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
 @Singleton
 public class MessagePaneExecutorService {
-  private final ThreadPoolExecutor executor = new ThreadPoolExecutor(5, 5, 5, TimeUnit.SECONDS, new LifoBlockingDeque<Runnable>());
+  private final ThreadPoolExecutor executor = new ThreadPoolExecutor(5, 5, 5, TimeUnit.SECONDS, new LifoBlockingDeque<Runnable>(), new ThreadFactory() {
+    private int threadNumber;
+
+    @Override
+    public Thread newThread(Runnable r) {
+      Thread thread = new Thread(r, "MessagePaneExecutorService-thread-" + ++threadNumber);
+      thread.setDaemon(true);
+      return thread;
+    }
+  });
+
   private final GroupWorker groupService = new GroupWorker();
 
   @Inject
@@ -30,63 +38,82 @@ public class MessagePaneExecutorService {
   }
 
   public synchronized void execute(final Task<?> task) {
-    groupService.queue.add(task);
-
-    if(!groupService.isRunning()) {
-      groupService.restart();
-    }
-
-    task.stateProperty().addListener(new ChangeListener<State>() {
-      @Override
-      public void changed(ObservableValue<? extends State> observable, State oldValue, State newValue) {
-        if(newValue == State.FAILED) {
-          System.out.println("[WARN] Worker " + task.getTitle() + " failed with exception: " + task.getException());
-        }
-      }
-    });
+    groupService.submit(task);
   }
 
   private class GroupWorker extends Service<Void> {
-    private final Queue<Task<?>> queue = new ConcurrentLinkedQueue<>();
+    private final List<Task<?>> activeTasks = new ArrayList<>();
 
-    private long submittedTaskCount;
+    private long startTaskCount;
+
+    public GroupWorker() {
+      Thread thread = new Thread() {
+        @Override
+        public void run() {
+          try {
+            for(;;) {
+              if(!activeTasks.isEmpty()) {
+                Platform.runLater(new Runnable() {
+                  @Override
+                  public void run() {
+                    synchronized(activeTasks) {
+                      if(!activeTasks.isEmpty() && !isRunning()) {
+                        restart();
+                      }
+                    }
+                  }
+                });
+              }
+
+              Thread.sleep(100);
+            }
+          }
+          catch(InterruptedException e) {
+            System.out.println("[FINE] " + this + " interrupted");
+          }
+        }
+      };
+
+      thread.setName("GroupWorker-activity-monitor");
+      thread.setDaemon(true);
+      thread.start();
+    }
+
+    public void submit(final Task<?> task) {
+      synchronized(activeTasks) {
+        activeTasks.add(task);
+        executor.execute(task);
+      }
+
+      task.stateProperty().addListener(new ChangeListener<State>() {
+        @Override
+        public void changed(ObservableValue<? extends State> observable, State oldValue, State newValue) {
+          if(newValue == State.FAILED) {
+            System.out.println("[WARN] Worker " + task.getTitle() + " failed with exception: " + task.getException());
+            task.getException().printStackTrace(System.out);
+          }
+        }
+      });
+    }
 
     @Override
     protected Task<Void> createTask() {
-      final long startTaskCount = submittedTaskCount;
-
       return new Task<Void>() {
         @Override
         protected Void call() throws Exception {
-          final List<Task<?>> tasks = new ArrayList<>();
-
           for(;;) {
-            synchronized(tasks) {
-              for(;;) {
-                Task<?> newTask = queue.poll();
-
-                if(newTask == null) {
-                  break;
-                }
-
-                tasks.add(newTask);
-                submittedTaskCount++;
-                executor.execute(newTask);
-              }
-            }
-
             Platform.runLater(new Runnable() {
               @Override
               public void run() {
                 long completed = executor.getCompletedTaskCount() - startTaskCount;
-                long total = submittedTaskCount - startTaskCount;
+                long total = executor.getTaskCount() - startTaskCount;
                 double totalProgress = 0.0;
                 double totalWork = 0.0;
                 String title = "";
                 String message = "";
 
-                synchronized(tasks) {
-                  for(Task<?> task : tasks) {
+                synchronized(activeTasks) {
+                  for(Task<?> task : activeTasks) {
                     String m = task.getMessage();
                     double p = task.getProgress();
 
@@ -108,14 +135,16 @@ public class MessagePaneExecutorService {
               }
             });
 
-            if(executor.getCompletedTaskCount() == submittedTaskCount) {
-              break;
+            synchronized(activeTasks) {
+              if(executor.getCompletedTaskCount() == executor.getTaskCount()) {
+                activeTasks.clear();
+                startTaskCount = executor.getTaskCount();
+                return null;
+              }
             }
 
             Thread.sleep(100);
           }
-
-          return null;
         }
       };
     }
