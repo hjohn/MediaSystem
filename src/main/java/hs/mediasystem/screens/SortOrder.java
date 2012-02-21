52aede02d98ac8ca95b728999e2b89dd8833ebb8
John Hendrikx <hjohn@xs4all.nl>
2012-02-21 01:00:48 +0100

    Added SortOrder

---
+++ src/main/java/hs/mediasystem/screens/SortOrder.java
@@ -0,0 +1,13 @@
+package hs.mediasystem.screens;
+
+public class SortOrder {
+  private final String title;
+
+  public SortOrder(String title) {
+    this.title = title;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+}
