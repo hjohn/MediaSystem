67a3f79fb7df88fc009c1d2dbe880221e7e4e04a
John Hendrikx <hjohn@xs4all.nl>
2012-03-02 11:47:24 +0100

    Added NodeList, a linked list with accessible nodes

---
+++ src/main/java/hs/mediasystem/util/NodeList.java
@@ -0,0 +1,45 @@
+package hs.mediasystem.util;
+
+public class NodeList<T> {
+  private Node head;
+
+  public synchronized Node addHead(T content) {
+    head = new Node(content, head, null);
+    return head;
+  }
+
+  public synchronized T removeHead() {
+    return unlink(head);
+  }
+
+  public synchronized T unlink(Node node) {
+    if(node == null) {
+      return null;
+    }
+
+    if(node.previous != null) {
+      node.previous.next = node.next;
+    }
+    if(node.next != null) {
+      node.next.previous = node.previous;
+    }
+    if(node == head) {
+      head = node.next;
+    }
+
+    return node.content;
+  }
+
+  public final class Node {
+    private final T content;
+
+    private Node next;
+    private Node previous;
+
+    private Node(T content, Node next, Node previous) {
+      this.content = content;
+      this.next = next;
+      this.previous = previous;
+    }
+  }
+}
