aec03c755b8c4f4e811584deda53776b798c6d8a
John Hendrikx <hjohn@xs4all.nl>
2012-03-03 12:25:53 +0100

    Specials now displayed as "Special" instead of null

--- src/main/java/hs/mediasystem/screens/SeasonAndEpisodeCellProvider.java
+++ src/main/java/hs/mediasystem/screens/SeasonAndEpisodeCellProvider.java
@@ -3,6 +3,7 @@ package hs.mediasystem.screens;
 import hs.mediasystem.framework.CellProvider;
 import hs.mediasystem.framework.MediaItem;
 import javafx.beans.binding.Bindings;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.scene.Node;
 
 public class SeasonAndEpisodeCellProvider implements CellProvider<MediaNode> {
@@ -14,7 +15,7 @@ public class SeasonAndEpisodeCellProvider implements CellProvider<MediaNode> {
 
     cell.titleProperty().bind(item.titleProperty());
     cell.subtitleProperty().set("");
-    cell.extraInfoProperty().bind(Bindings.convert(item.episodeProperty()));
+    cell.extraInfoProperty().bind(Bindings.when(item.episodeProperty().isNull()).then(new SimpleStringProperty("Special")).otherwise(Bindings.convert(item.episodeProperty())));
     cell.ratingProperty().bind(item.ratingProperty().divide(10));
 
     return cell;
