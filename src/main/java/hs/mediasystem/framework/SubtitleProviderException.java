5b32f81667cbf0781d773fbaa96221098a400780
John Hendrikx <hjohn@xs4all.nl>
2012-03-13 01:29:11 +0100

    Added constructor with exception cause

--- src/main/java/hs/mediasystem/framework/SubtitleProviderException.java
+++ src/main/java/hs/mediasystem/framework/SubtitleProviderException.java
@@ -1,5 +1,11 @@
 package hs.mediasystem.framework;
 
 public class SubtitleProviderException extends Exception {
+  public SubtitleProviderException() {
+    super();
+  }
 
+  public SubtitleProviderException(Throwable cause) {
+    super(cause);
+  }
 }
