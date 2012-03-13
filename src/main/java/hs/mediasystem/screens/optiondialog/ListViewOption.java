7fdf6799e5f5fcb06d3a7fc9be5284882dbeedc9
John Hendrikx <hjohn@xs4all.nl>
2012-03-13 12:24:02 +0100

    Cursor up/down will now exit the ListView when top/bottom is reached

--- src/main/java/hs/mediasystem/screens/optiondialog/ListViewOption.java
+++ src/main/java/hs/mediasystem/screens/optiondialog/ListViewOption.java
@@ -3,16 +3,22 @@ package hs.mediasystem.screens.optiondialog;
 import hs.mediasystem.util.StringConverter;
 import javafx.beans.property.ObjectProperty;
 import javafx.collections.ObservableList;
+import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyCodeCombination;
+import javafx.scene.input.KeyCombination;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.VBox;
 import javafx.util.Callback;
 
 public class ListViewOption<T> extends Option {
+  private static final KeyCombination UP = new KeyCodeCombination(KeyCode.UP);
+  private static final KeyCombination DOWN = new KeyCodeCombination(KeyCode.DOWN);
+
   private final ListView<T> listView = new ListView<>();
 
   public ListViewOption(final String description, final ObjectProperty<T> property, ObservableList<T> items, final StringConverter<T> stringConverter) {
@@ -49,12 +55,23 @@ public class ListViewOption<T> extends Option {
       }
     });
 
-    listView.addEventHandler(KeyEvent.KEY_PRESSED, new EventHandler<KeyEvent>() {
+    listView.setOnKeyPressed(new EventHandler<KeyEvent>() {
       @Override
       public void handle(KeyEvent event) {
         if(event.getCode() == KeyCode.ENTER) {
           if(property != null) {
             property.set(listView.getSelectionModel().getSelectedItem());
+          }
+          event.consume();
+        }
+        else if(UP.match(event)) {
+          if(listView.getFocusModel().getFocusedIndex() == 0) {
+            Event.fireEvent(listView.getParent(), event);
+          }
+        }
+        else if(DOWN.match(event)) {
+          if(listView.getFocusModel().getFocusedIndex() == listView.getItems().size() - 1) {
+            Event.fireEvent(listView.getParent(), event);
           }
         }
       }
@@ -64,6 +81,8 @@ public class ListViewOption<T> extends Option {
   @Override
   public void requestFocus() {
     listView.requestFocus();
-    listView.getFocusModel().focusNext();
+    if(listView.getFocusModel().getFocusedIndex() == -1) {
+      listView.getFocusModel().focusNext();
+    }
   }
 }
