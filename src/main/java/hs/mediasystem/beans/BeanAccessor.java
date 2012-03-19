be15416df04554424f7d3e17a76417f496a3341d
John Hendrikx <hjohn@xs4all.nl>
2012-03-19 21:03:13 +0100

    BeanAccessor can now access nested properties

--- src/main/java/hs/mediasystem/beans/BeanAccessor.java
+++ src/main/java/hs/mediasystem/beans/BeanAccessor.java
@@ -2,58 +2,80 @@ package hs.mediasystem.beans;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.Arrays;
 
 public class BeanAccessor<T> implements Accessor<T> {
   private final Object instance;
-  private final String propertyName;
+  private final String[] propertyNames;
+  private final Method[] readMethods;
+  private final String writeMethodName;
 
-  private Method readMethod;
   private Method writeMethod;
 
-  public BeanAccessor(Object instance, String propertyName) {
+  public BeanAccessor(String writeMethodName, Object instance, String... propertyNames) {
+    this.writeMethodName = writeMethodName == null ? propertyNames[propertyNames.length - 1] : writeMethodName;
     this.instance = instance;
-    this.propertyName = propertyName;
+    this.propertyNames = propertyNames;
+
+    readMethods = new Method[propertyNames.length];
   }
 
-  private Method getReadMethod() {
-    if(readMethod == null) {
-      readMethod = BeanUtils.getReadMethod(instance.getClass(), propertyName);
+  public BeanAccessor(Object instance, String... propertyNames) {
+    this(null, instance, propertyNames);
+  }
+
+  private Object findCurrentInstance() throws IllegalAccessException, InvocationTargetException {
+    Object currentInstance = instance;
+    int index = 0;
+
+    while(currentInstance != null) {
+      if(readMethods[index] == null) {
+        readMethods[index] = BeanUtils.getReadMethod(currentInstance.getClass(), propertyNames[index]);
+      }
+
+      if(index == propertyNames.length - 1) {
+        break;
+      }
+
+      currentInstance = readMethods[index++].invoke(currentInstance);
     }
 
-    return readMethod;
-  }
-
-  private Class<?> getType() {
-    return getReadMethod().getReturnType();
+    return currentInstance;
   }
 
   @SuppressWarnings("unchecked")
   @Override
   public T read() {
     try {
-      return (T)getReadMethod().invoke(instance);
+      Object currentInstance = findCurrentInstance();
+
+      if(currentInstance != null) {
+        return (T)readMethods[propertyNames.length - 1].invoke(currentInstance);
+      }
     }
-    catch(IllegalAccessException e) {
+    catch(InvocationTargetException | IllegalAccessException e) {
       throw new RuntimeException(e);
     }
-    catch(InvocationTargetException e) {
-      throw new RuntimeException(e);
-    }
+
+    return null;
   }
 
   @Override
   public void write(T value) {
-    if(writeMethod == null) {
-      writeMethod = BeanUtils.getWriteMethod(instance.getClass(), propertyName, getType());
-    }
-
     try {
-      writeMethod.invoke(instance, value);
+      Object currentInstance = findCurrentInstance();
+
+      if(currentInstance == null) {
+        throw new RuntimeException("property " + Arrays.toString(propertyNames) + " of " + instance + " cannot be written.");
+      }
+
+      if(writeMethod == null) {
+        writeMethod = BeanUtils.getWriteMethod(currentInstance.getClass(), writeMethodName, readMethods[propertyNames.length - 1].getReturnType());
+      }
+
+      writeMethod.invoke(currentInstance, value);
     }
-    catch(IllegalAccessException e) {
-      throw new RuntimeException(e);
-    }
-    catch(InvocationTargetException e) {
+    catch(InvocationTargetException | IllegalAccessException e) {
       throw new RuntimeException(e);
     }
   }
