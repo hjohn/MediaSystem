af0a3f00db8ea316994dd9ce34af364da3be2e57
John Hendrikx <hjohn@xs4all.nl>
2012-05-25 17:40:49 +0200

    Removed identifier table, changed fields to varchar and removed some PostgreSQL specific syntax

--- src/main/resources/hs/mediasystem/db/db-v0001.sql
+++ src/main/resources/hs/mediasystem/db/db-v0001.sql
@@ -1,36 +1,36 @@
-CREATE TABLE IF NOT EXISTS dbinfo
+CREATE TABLE dbinfo
 (
-  name character varying(50) NOT NULL,
-  value character varying(50) NOT NULL,
+  name varchar(50) NOT NULL,
+  value varchar(50) NOT NULL,
   
   CONSTRAINT dbinfo_pk PRIMARY KEY (name)
 );
 
 INSERT INTO dbinfo (name, value) VALUES ('version', 0);
 
-CREATE TABLE IF NOT EXISTS items
+CREATE TABLE items
 (
   id serial4,
-  "type" character varying(10) NOT NULL,
-  provider character varying(20) NOT NULL,
-  providerid character varying(20) NOT NULL,
+  "type" varchar(10) NOT NULL,
+  provider varchar(20) NOT NULL,
+  providerid varchar(20) NOT NULL,
   
-  title character varying(100) NOT NULL,
-  subtitle character varying(100),
+  title varchar(100) NOT NULL,
+  subtitle varchar(100),
   season integer,
   episode integer,
   releasedate date,
   releaseyear integer,
-  plot character varying(2000),
-  imdbid character varying(20),
+  plot varchar(2000),
+  imdbid varchar(20),
   rating numeric(4,1),
   runtime integer,
-  genres character varying(100),
-  language character varying(20),
-  tagline character varying(200),
-  backgroundurl character varying(1000),
-  bannerurl character varying(1000),
-  posterurl character varying(1000),
+  genres varchar(100),
+  language varchar(20),
+  tagline varchar(200),
+  backgroundurl varchar(1000),
+  bannerurl varchar(1000),
+  posterurl varchar(1000),
   
   background bytea,
   banner bytea,
@@ -43,14 +43,4 @@ CREATE TABLE IF NOT EXISTS items
   
   CONSTRAINT items_id PRIMARY KEY (id),
   CONSTRAINT items_providerkey UNIQUE ("type", provider, providerid)
-);
-
-CREATE TABLE IF NOT EXISTS identifiers
-(
-  surrogatename character varying(250) NOT NULL,
-  "type" character varying(10) NOT NULL,
-  provider character varying(20) NOT NULL,
-  providerid character varying(20) NOT NULL,
-  
-  CONSTRAINT identifiers_surrogatename PRIMARY KEY (surrogatename)
 );
