db7a58177303f536618e0848c2272d000ea815af
John Hendrikx <hjohn@xs4all.nl>
2012-06-01 00:28:50 +0200

    Introduction of Parameters class and companion classes to simplify passing of parameters to Enrichers

---
+++ src/main/java/hs/mediasystem/enrich/Parameters.java
@@ -0,0 +1,25 @@
+package hs.mediasystem.enrich;
+
+import java.util.Map;
+
+public class Parameters {
+  private final Map<Class<?>, Object> parameterMap;
+
+  public Parameters(Map<Class<?>, Object> parameterMap) {
+    this.parameterMap = parameterMap;
+  }
+
+  @SuppressWarnings("unchecked")
+  public <T> T get(Class<T> cls) {
+    return (T)parameterMap.get(cls);
+  }
+
+  @SuppressWarnings("unchecked")
+  public <T> T unwrap(Class<? extends WrappedValue<T>> cls) {
+    return ((WrappedValue<T>)parameterMap.get(cls)).get();
+  }
+
+  public interface WrappedValue<V> {
+    public V get();
+  }
+}
