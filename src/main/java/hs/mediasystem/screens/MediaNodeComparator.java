f2cc7d1653b65b2bc43c08262338aa872f035eaa
John Hendrikx <hjohn@xs4all.nl>
2012-06-02 13:45:00 +0200

    Cleaned up

--- src/main/java/hs/mediasystem/screens/MediaNodeComparator.java
+++ src/main/java/hs/mediasystem/screens/MediaNodeComparator.java
@@ -7,16 +7,6 @@ public class MediaNodeComparator implements Comparator<MediaNode> {
 
   @Override
   public int compare(MediaNode o1, MediaNode o2) {
-    int result = o1.getMedia().getTitle().compareTo(o2.getMedia().getTitle());
-
-    if(result == 0) {
-      //result = Integer.compare(o1.getSeason() != null ? o1.getSeason() : Integer.MAX_VALUE, o2.getSeason() != null ? o2.getSeason() : Integer.MAX_VALUE);
-
-      if(result == 0) {
-        // result = o1.getSubtitle().compareTo(o2.getSubtitle());
-      }
-    }
-
-    return result;
+    return o1.getMedia().getTitle().compareTo(o2.getMedia().getTitle());
   }
 }
