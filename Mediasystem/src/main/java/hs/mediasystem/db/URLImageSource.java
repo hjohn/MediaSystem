67d11ff270d5f2d2b4351b9a49e682ca42a3cc70
John Hendrikx <hjohn@xs4all.nl>
2012-06-08 11:36:58 +0200

    Added log print

--- Mediasystem/src/main/java/hs/mediasystem/db/URLImageSource.java
+++ Mediasystem/src/main/java/hs/mediasystem/db/URLImageSource.java
@@ -14,6 +14,7 @@ public class URLImageSource implements Source<byte[]> {
 
   @Override
   public byte[] get() {
+    System.out.println("[FINE] URLImageSource.get() - Downloading '" + url + "'");
     return Downloader.tryReadURL(url);
   }
 }
