5f72daaca09c85296084e5ea6925f507e1a2657f
John Hendrikx <hjohn@xs4all.nl>
2012-06-20 10:47:15 +0200

    Castings are now added to the Media object

--- Mediasystem/src/main/java/hs/mediasystem/framework/AbstractEnrichTaskProvider.java
+++ Mediasystem/src/main/java/hs/mediasystem/framework/AbstractEnrichTaskProvider.java
@@ -1,11 +1,17 @@
 package hs.mediasystem.framework;
 
+import hs.mediasystem.dao.Casting;
 import hs.mediasystem.dao.Item;
 import hs.mediasystem.dao.ItemNotFoundException;
 import hs.mediasystem.dao.ItemsDao;
 import hs.mediasystem.dao.MediaData;
 import hs.mediasystem.dao.TypeBasedItemEnricher;
 import hs.mediasystem.enrich.EnrichTask;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 public abstract class AbstractEnrichTaskProvider<T extends Media> {
   private final String title;
@@ -41,7 +47,11 @@ public abstract class AbstractEnrichTaskProvider<T extends Media> {
             return null;
           }
 
-          return itemToEnrichType(item);
+          T enrichType = itemToEnrichType(item);
+
+          enrichType.castingsProperty().get().addAll(getOrderedCastings(item));
+
+          return enrichType;
         }
         catch(ItemNotFoundException e) {
           return null;
@@ -92,12 +102,34 @@ public abstract class AbstractEnrichTaskProvider<T extends Media> {
 
         T enrichType = itemToEnrichType(item);
 
-        enrichType.castingsProperty().get().addAll(item.getCastings());
+        enrichType.castingsProperty().get().addAll(getOrderedCastings(item));
 
         updateProgress(4, 4);
 
         return enrichType;
       }
     };
+  }
+
+  private static List<Casting> getOrderedCastings(Item item) {
+    List<Casting> castings = new ArrayList<>(item.getCastings());
+
+    Collections.sort(castings, new Comparator<Casting>() {
+      @Override
+      public int compare(Casting o1, Casting o2) {
+        int result = Integer.compare(o1.getIndex(), o2.getIndex());
+
+        if(result == 0) {
+          String c1 = o1.getCharacterName() == null ? "" : o1.getCharacterName();
+          String c2 = o2.getCharacterName() == null ? "" : o2.getCharacterName();
+
+          result = c1.compareTo(c2);
+        }
+
+        return result;
+      }
+    });
+
+    return castings;
   }
 }
