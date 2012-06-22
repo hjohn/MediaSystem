d52ed7f846bd5e0e061f078dd0095e60c29f1627
John Hendrikx <hjohn@xs4all.nl>
2012-06-22 07:56:08 +0200

    Added Image pojo

---
+++ Mediasystem/src/main/java/hs/mediasystem/dao/Image.java
@@ -0,0 +1,38 @@
+package hs.mediasystem.dao;
+
+import hs.mediasystem.db.Column;
+import hs.mediasystem.db.Table;
+
+@Table(name = "images")
+public class Image {
+
+  @Column
+  private String url;
+
+  @Column
+  private byte[] image;
+
+  public Image(String url, byte[] data) {
+    this.url = url;
+    this.image = data;
+  }
+
+  public Image() {
+  }
+
+  public String getUrl() {
+    return url;
+  }
+
+  public void setUrl(String url) {
+    this.url = url;
+  }
+
+  public byte[] getImage() {
+    return image;
+  }
+
+  public void setImage(byte[] image) {
+    this.image = image;
+  }
+}
