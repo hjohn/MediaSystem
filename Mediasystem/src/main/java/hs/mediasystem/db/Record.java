c63443d5aa024210c4d72cdd72ce7590d721cebd
John Hendrikx <hjohn@xs4all.nl>
2012-06-23 16:59:16 +0200

    Added additional convience methods

--- Mediasystem/src/main/java/hs/mediasystem/db/Record.java
+++ Mediasystem/src/main/java/hs/mediasystem/db/Record.java
@@ -20,15 +20,27 @@ public class Record {
     return (String)get(fieldName);
   }
 
+  public Boolean getBoolean(String fieldName) {
+    return (Boolean)get(fieldName);
+  }
+
   public Integer getInteger(String fieldName) {
     return (Integer)get(fieldName);
   }
 
-  public Date getDate(String fieldName) {
-    return (Date)get(fieldName);
+  public Long getLong(String fieldName) {
+    return (Long)get(fieldName);
   }
 
   public Float getFloat(String fieldName) {
     return (Float)get(fieldName);
+  }
+
+  public byte[] getBytes(String fieldName) {
+    return (byte[])get(fieldName);
+  }
+
+  public Date getDate(String fieldName) {
+    return (Date)get(fieldName);
   }
 }
