35c0caf491cceeb65bc61397e81839a3c8503ad8
John Hendrikx <hjohn@xs4all.nl>
2012-06-27 11:05:59 +0200

    Changed Nullable retention to runtime so Guice can find it

--- Mediasystem/src/main/java/hs/mediasystem/util/annotation/Nullable.java
+++ Mediasystem/src/main/java/hs/mediasystem/util/annotation/Nullable.java
@@ -6,7 +6,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Target({ElementType.FIELD, java.lang.annotation.ElementType.METHOD, ElementType.PARAMETER, ElementType.LOCAL_VARIABLE})
-@Retention(RetentionPolicy.CLASS)
+@Retention(RetentionPolicy.RUNTIME)
 public @interface Nullable {
 
 }
