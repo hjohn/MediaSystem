172a5f995411985e335dc9a65d26cc5ec02d1469
John Hendrikx <hjohn@xs4all.nl>
2012-07-14 19:16:50 +0200

    Added option to configure movie paths

--- hs.mediasystem.ext/src/hs/mediasystem/ext/media/movie/Activator.java
+++ hs.mediasystem.ext/src/hs/mediasystem/ext/media/movie/Activator.java
@@ -8,16 +8,26 @@ import hs.mediasystem.framework.Media;
 import hs.mediasystem.framework.MediaItem;
 import hs.mediasystem.framework.SubtitleCriteriaProvider;
 import hs.mediasystem.persist.PersistQueue;
+import hs.mediasystem.screens.ConfigurationOption;
 import hs.mediasystem.screens.DefaultMediaGroup;
 import hs.mediasystem.screens.MainMenuExtension;
 import hs.mediasystem.screens.MediaGroup;
 import hs.mediasystem.screens.MediaNodeCell;
 import hs.mediasystem.screens.MediaNodeCellProvider;
+import hs.mediasystem.screens.optiondialog.PathSelectOption;
+import hs.mediasystem.screens.optiondialog.Option;
+import hs.mediasystem.screens.optiondialog.SubOption;
 import hs.mediasystem.screens.selectmedia.SelectMediaPresentationProvider;
+import hs.mediasystem.util.Callable;
 
+import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Hashtable;
+import java.util.List;
 import java.util.Map;
+
+import javafx.beans.property.SimpleObjectProperty;
 
 import org.apache.felix.dm.DependencyActivatorBase;
 import org.apache.felix.dm.DependencyManager;
@@ -27,6 +37,84 @@ public class Activator extends DependencyActivatorBase {
 
   @Override
   public void init(BundleContext context, DependencyManager manager) throws Exception {
+    manager.add(createComponent()
+      .setInterface(ConfigurationOption.class.getName(), null)
+      .setImplementation(new ConfigurationOption() {
+        @Override
+        public double order() {
+          return 0;
+        }
+
+        @Override
+        public String getTitle() {
+          return "Movies";
+        }
+
+        @Override
+        public String getParentId() {
+          return null;
+        }
+
+        @Override
+        public String getId() {
+          return "movies";
+        }
+
+        @Override
+        public Option createOption() {
+          return null;
+        }
+      })
+    );
+
+    manager.add(createComponent()
+      .setInterface(ConfigurationOption.class.getName(), null)
+      .setImplementation(new ConfigurationOption() {
+        @Override
+        public double order() {
+          return 0;
+        }
+
+        @Override
+        public String getTitle() {
+          return "Add/Remove folder";
+        }
+
+        @Override
+        public String getParentId() {
+          return "movies";
+        }
+
+        @Override
+        public String getId() {
+          return "movies";
+        }
+
+        @Override
+        public Option createOption() {
+          return new SubOption(getTitle(), new Callable<List<Option>>() {
+            @Override
+            public List<Option> call() {
+              List<Option> options = new ArrayList<>();
+
+              options.add(new SubOption("Add folder", new Callable<List<Option>>() {
+                @Override
+                public List<Option> call() {
+                  List<Option> options = new ArrayList<>();
+
+                  options.add(new PathSelectOption("Select folder", new SimpleObjectProperty<Path>(), PathSelectOption.ONLY_DIRECTORIES_FILTER));
+
+                  return options;
+                }
+              }));
+
+              return options;
+            }
+          });
+        }
+      })
+    );
+
     manager.add(createComponent()
       .setInterface(MediaNodeCellProvider.class.getName(), new Hashtable<String, Object>() {{
         put("mediasystem.class", Movie.class);
