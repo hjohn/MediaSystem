0ae714fc1e1f580156513df24a93d6ae4eda4789
John Hendrikx <hjohn@xs4all.nl>
2012-07-15 20:33:43 +0200

    Added new Dialog implementation that does not use a Stage

---
+++ Mediasystem/src/main/java/hs/mediasystem/util/DialogPane.java
@@ -0,0 +1,69 @@
+package hs.mediasystem.util;
+
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
+import javafx.scene.Parent;
+import javafx.scene.effect.ColorAdjust;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+public class DialogPane extends StackPane implements Dialog {
+  private final StackPane stackPane = new StackPane();
+
+  private Stage owner;
+
+  protected void setParentEffect(Parent parent) {
+    ColorAdjust colorAdjust = new ColorAdjust();
+
+    Timeline fadeOut = new Timeline(
+      new KeyFrame(Duration.ZERO,
+        new KeyValue(colorAdjust.brightnessProperty(), 0)
+      ),
+      new KeyFrame(Duration.seconds(1),
+        new KeyValue(colorAdjust.brightnessProperty(), -0.5)
+      )
+    );
+
+    parent.setEffect(colorAdjust);
+
+    fadeOut.play();
+  }
+
+  protected void removeParentEffect(Parent parent) {
+    parent.setEffect(null);
+  }
+
+  @Override
+  public void showDialog(Stage parentStage, boolean synchronous) {
+    if(synchronous) {
+      throw new UnsupportedOperationException();
+    }
+
+    this.owner = parentStage;
+
+    Parent root = parentStage.getScene().getRoot();
+    parentStage.getScene().setRoot(stackPane);
+
+    stackPane.getChildren().add(root);
+    stackPane.getChildren().add(this);
+
+    setParentEffect(root);
+    requestFocus();
+  }
+
+  public void close() {
+    Parent originalRoot = (Parent)stackPane.getChildren().remove(0);
+
+    owner.getScene().setRoot(originalRoot);
+    removeParentEffect(originalRoot);
+
+    owner = null;
+  }
+
+  @Override
+  public void requestFocus() {
+    getChildren().get(0).requestFocus();
+  }
+}
