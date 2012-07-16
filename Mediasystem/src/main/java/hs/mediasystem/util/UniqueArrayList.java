139ae05509ed2dba5a1b82926824641ba5e19c4d
John Hendrikx <hjohn@xs4all.nl>
2012-07-16 23:21:26 +0200

    Fixed addAll with index to work as expected when duplicates are present -- the default implementation of AbstractList did not suffice

--- Mediasystem/src/main/java/hs/mediasystem/util/UniqueArrayList.java
+++ Mediasystem/src/main/java/hs/mediasystem/util/UniqueArrayList.java
@@ -2,6 +2,7 @@ package hs.mediasystem.util;
 
 import java.util.AbstractList;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -11,6 +12,13 @@ import java.util.Set;
  *
  * It is not allowed to set an index to a new value as its behaviour when set would result
  * in a duplicate element is hard to define.
+ *
+ * Note that addAll with index can work a bit unexpected.  When adding a collection which
+ * contains elements already in this UniqueList then this element will be skipped --
+ * therefore the element already in the list will retain its position.
+ *
+ * For example, consider a list: D, E, F, B.  Adding a list A, B, C at index 0 would allow A
+ * and C to be inserted, but not B.  The final result would be: A, C, D, E, F, B.
  *
  * As a result of the unique constraint, the contains method performs faster than for
  * regular lists.
@@ -43,9 +51,31 @@ public class UniqueArrayList<T> extends AbstractList<T> implements UniqueList<T>
 
   @Override
   public void add(int index, T element) {
+    addInternal(index, element);
+  }
+
+  public boolean addInternal(int index, T element) {
     if(backingSet.add(element)) {
       backingList.add(index, element);
+      return true;
     }
+
+    return false;
+  }
+
+  @Override
+  public boolean addAll(int index, Collection<? extends T> c) {
+    int i = index;
+    boolean modified = false;
+
+    for(T element : c) {
+      if(addInternal(i, element)) {
+        i++;
+        modified = true;
+      }
+    }
+
+    return modified;
   }
 
   @Override
