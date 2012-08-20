f79037498eeafeddd74030256d31b170321132cc
John Hendrikx <hjohn@xs4all.nl>
2012-08-20 02:03:36 +0200

    Added method to only configure an existing GridPane

--- Mediasystem/src/main/java/hs/mediasystem/util/GridPaneUtil.java
+++ Mediasystem/src/main/java/hs/mediasystem/util/GridPaneUtil.java
@@ -5,9 +5,7 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.RowConstraints;
 
 public class GridPaneUtil {
-  public static GridPane create(double[] columns, double[] rows) {
-    GridPane gridPane = new GridPane();
-
+  public static void configure(GridPane gridPane, double[] columns, double[] rows) {
     for(double column : columns) {
       ColumnConstraints constraints = new ColumnConstraints();
 
@@ -23,6 +21,12 @@ public class GridPaneUtil {
 
       gridPane.getRowConstraints().add(constraints);
     }
+  }
+
+  public static GridPane create(double[] columns, double[] rows) {
+    GridPane gridPane = new GridPane();
+
+    configure(gridPane, columns, rows);
 
     return gridPane;
   }
