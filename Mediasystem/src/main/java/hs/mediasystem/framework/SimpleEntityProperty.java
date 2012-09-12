edf0cfaa2829d54c280e4a72ded97b7911459b46
John Hendrikx <hjohn@xs4all.nl>
2012-09-12 11:48:46 +0200

    Added property with optional enricher

---
+++ Mediasystem/src/main/java/hs/mediasystem/framework/SimpleEntityProperty.java
@@ -0,0 +1,21 @@
+package hs.mediasystem.framework;
+
+import hs.mediasystem.enrich.InstanceEnricher;
+import javafx.beans.property.SimpleObjectProperty;
+
+public class SimpleEntityProperty<T> extends SimpleObjectProperty<T> {
+  private InstanceEnricher<?, ?> enricher;
+
+  public SimpleEntityProperty(Object bean, String name) {
+    super(bean, name);
+  }
+
+  public <E> void setEnricher(InstanceEnricher<E, ?> enricher) {
+    this.enricher = enricher;
+  }
+
+  @SuppressWarnings("unchecked")
+  public <E> InstanceEnricher<E, T> getEnricher() {
+    return (InstanceEnricher<E, T>)enricher;
+  }
+}
