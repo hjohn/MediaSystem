46de20be2a7360bdfd4cccc04aa0f47d752121bf
John Hendrikx <hjohn@xs4all.nl>
2013-06-05 09:14:17 +0200

    Added javadoc

--- Mediasystem/src/main/java/hs/mediasystem/screens/selectmedia/DetailPaneDecorator.java
+++ Mediasystem/src/main/java/hs/mediasystem/screens/selectmedia/DetailPaneDecorator.java
@@ -4,5 +4,9 @@ import javafx.beans.property.ObjectProperty;
 
 public interface DetailPaneDecorator<T> {
   void decorate(boolean interactive);
+
+  /**
+   * Property for the data used for the decorated content this decorator provides.
+   */
   ObjectProperty<T> dataProperty();
 }
