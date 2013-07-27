601b050633a5bafc3afa427d1ca0398822429b50
John Hendrikx <hjohn@xs4all.nl>
2013-07-27 22:47:25 +0200

    Extended NavigationEvent with NAVIGATION_EXIT to differentiate between a back to parent and a exit to previous level navigation event

--- Mediasystem/src/main/java/hs/mediasystem/screens/NavigationEvent.java
+++ Mediasystem/src/main/java/hs/mediasystem/screens/NavigationEvent.java
@@ -5,12 +5,16 @@ import javafx.event.EventType;
 
 public class NavigationEvent extends Event {
   public static final EventType<NavigationEvent> ANY = new EventType<>(EventType.ROOT, "NAVIGATION");
-  public static final EventType<NavigationEvent> NAVIGATION_BACK = new EventType<>(ANY, "NAVIGATION_BACK");
+
   public static final EventType<NavigationEvent> NAVIGATION_LEFT = new EventType<>(ANY, "NAVIGATION_LEFT");
   public static final EventType<NavigationEvent> NAVIGATION_RIGHT = new EventType<>(ANY, "NAVIGATION_RIGHT");
   public static final EventType<NavigationEvent> NAVIGATION_UP = new EventType<>(ANY, "NAVIGATION_UP");
   public static final EventType<NavigationEvent> NAVIGATION_DOWN = new EventType<>(ANY, "NAVIGATION_DOWN");
   public static final EventType<NavigationEvent> NAVIGATION_SELECT = new EventType<>(ANY, "NAVIGATION_SELECT");
+  public static final EventType<NavigationEvent> NAVIGATION_ANCESTOR = new EventType<>(ANY, "NAVIGATION_ANCESTOR");
+
+  public static final EventType<NavigationEvent> NAVIGATION_EXIT = new EventType<>(NAVIGATION_ANCESTOR, "NAVIGATION_EXIT");
+  public static final EventType<NavigationEvent> NAVIGATION_BACK = new EventType<>(NAVIGATION_ANCESTOR, "NAVIGATION_BACK");
 
   public NavigationEvent(EventType<NavigationEvent> navigationEvent) {
     super(navigationEvent);
