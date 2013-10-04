ecbcd2ba224d67fee44abb206e285df74b6eebc1
John Hendrikx <hjohn@xs4all.nl>
2013-10-04 06:34:19 +0200

    Fixed old style JUnit import

--- Mediasystem/src/test/java/hs/mediasystem/framework/NameDecoderTest.java
+++ Mediasystem/src/test/java/hs/mediasystem/framework/NameDecoderTest.java
@@ -7,7 +7,7 @@ import hs.mediasystem.framework.NameDecoder.Hint;
 import java.util.Arrays;
 import java.util.Collection;
 
-import junit.framework.Assert;
+import org.junit.Assert;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
