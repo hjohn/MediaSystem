b46a0898079daf39fa665a69292721dd14f11a5d
John Hendrikx <hjohn@xs4all.nl>
2013-12-31 10:09:25 +0100

    Added a stack trace when slow event handling occurs

--- Mediasystem/src/main/java/hs/mediasystem/util/SceneUtil.java
+++ Mediasystem/src/main/java/hs/mediasystem/util/SceneUtil.java
@@ -1,5 +1,10 @@
 package hs.mediasystem.util;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.event.Event;
@@ -10,6 +15,8 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 
 public class SceneUtil {
+  private static final ScheduledExecutorService EVENT_TIMEOUT_EXECUTOR = Executors.newSingleThreadScheduledExecutor();
+
   public static Scene createScene(Parent root) {
     Scene scene = new Scene(root);
 
@@ -19,6 +26,18 @@ public class SceneUtil {
       @Override
       public Event dispatchEvent(Event event, EventDispatchChain tail) {
         long millis = System.currentTimeMillis();
+        Thread fxThread = Thread.currentThread();
+
+        ScheduledFuture<?> future = EVENT_TIMEOUT_EXECUTOR.schedule(new Runnable() {
+          @Override
+          public void run() {
+            System.out.println("[WARN] Slow Event Handling, trace:");
+
+            for(StackTraceElement element : fxThread.getStackTrace()) {
+              System.out.println("[WARN]   -- " + element);
+            }
+          }
+        }, 1000, TimeUnit.MILLISECONDS);
 
         Event returnedEvent = eventDispatcher.dispatchEvent(event, tail);
 
@@ -27,6 +46,8 @@ public class SceneUtil {
         if(millis >= 100) {
           System.out.println("[WARN] Slow Event Handling: " + millis + " ms for event: " + event);
         }
+
+        future.cancel(false);
 
         return returnedEvent;
       }
