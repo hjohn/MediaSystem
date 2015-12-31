83dd78daece338740988ee05007b0e5d11724c3f
John Hendrikx <hjohn@xs4all.nl>
2015-12-31 20:53:16 +0100

    Upgraded to VLCJ 3.5.0

--- mediasystem-ext-player-vlc/src/main/java/hs/mediasystem/ext/player/vlc/AbstractVLCPlayerFactory.java
+++ mediasystem-ext-player-vlc/src/main/java/hs/mediasystem/ext/player/vlc/AbstractVLCPlayerFactory.java
@@ -14,8 +14,6 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
-import uk.co.caprica.vlcj.logger.Logger;
-import uk.co.caprica.vlcj.logger.Logger.Level;
 import javafx.beans.property.ObjectProperty;
 
 import com.sun.jna.NativeLibrary;
@@ -45,7 +43,6 @@ public abstract class AbstractVLCPlayerFactory implements PlayerFactory {
     }
 
     NativeLibrary.addSearchPath("libvlc", libVlcPathProperty.get().toString());
-    Logger.setLevel(Level.INFO);
 
     List<String> args = new ArrayList<>();
     Section vlcArgsSection = ini.getSection("vlc.args");
